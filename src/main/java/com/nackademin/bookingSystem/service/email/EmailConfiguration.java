dbcd9894fae2d4ce8a5f403d7b4422e61d6a4f42
hodeiez <hodeinaiz@hotmail.com>
2021-08-07 00:04:25 +0200

    again EmailConfiguration

--- src/main/java/com/nackademin/bookingSystem/service/email/EmailConfiguration.java
+++ src/main/java/com/nackademin/bookingSystem/service/email/EmailConfiguration.java
@@ -2,6 +2,7 @@ package com.nackademin.bookingSystem.service.email;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -15,6 +16,7 @@ import java.util.Properties;
  * Project: BookingSystem
  * Copyright: MIT
  */
+@Configuration
 @PropertySource(value={"classpath:application-local.yml"})
 public class EmailConfiguration {
 
